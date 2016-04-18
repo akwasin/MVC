1e162820d16ae9a7e36b08afc74286b60ee8092e
akwasin <akwasiijunior@gmail.com>
2016-04-18 14:26:04 +0200

    uppstart

---
+++ MVC_Uppgift2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Uppgift2.MvcApplication" Language="C#" %>
---
+++ MVC_Uppgift2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Uppgift2.Startup))]
+namespace MVC_Uppgift2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
