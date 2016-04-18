66a416ccb8f8f360bb6103d4ad839575f196d230
akwasin <akwasiijunior@gmail.com>
2016-04-18 15:05:46 +0200

    Uppstart uppg 4

--- MVC_Uppgift3/App_Start/RouteConfig.cs
+++ MVC_Uppgift3/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace MVC_Uppgift3
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
